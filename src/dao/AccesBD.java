4afe6f09feee211c5858c9b5d5782e7d81e2b2bf
pierre1907 <saint-pierre.kassi@ism.edu.sn>
2022-12-04 12:48:57 +0000

    first update of december 4th 2022

--- src/dao/AccesBD.java
+++ src/dao/AccesBD.java
@@ -7,23 +7,80 @@ public class AccesBD {
     private ArrayList<Client> listecli = new ArrayList<Client>();
     private ArrayList<Agence> listeag = new ArrayList<Agence>();
 
+    //recherche d'une agence
     public Agence searchAgence(String numag) {
         Agence ag=null;
         for (Agence x: listeag){
             if (x.getCodeag().equals(numag))
                 ag=x;
+            break;
         }
         return ag;
     }
 
-    private  void saveClient(Client c, String numag){
-        Agence ag = searchAgence(numag);
-        if (ag == null)
-            System.out.println("Numero d'agence inexistant !!!");
+    //creation de client
+    public void saveClient(Client cl,Agence ag){
+        cl.setAgence(ag);
+        ag.getLclients().add(cl);
+        listecli.add(cl);
+    }
+
+    //creation d'agence
+    public void createAg(Agence ag){
+        listeag.add(ag);
+    }
+
+    //lister les agences
+    public void listAg(){
+        for (Agence t : listeag)
+        {
+            System.out.println("________________________________");
+            System.out.println("Code agence: " +t.getCodeag());
+            System.out.println("Nom agence: "+t.getNomag());
+        }
+        System.out.println("________________________________");
+
+    }
+
+    //lister les clients
+    public void listCl(String numag ){
+        Agence ag= searchAgence(numag);
+        if(ag==null)
+            System.out.println("Numero d'agence inexistant !!!!");
         else {
-            c.setAgence(ag);
-            ag.addClient(c);
-            listecli.add(c);
+            ArrayList<Client> lc = ag.getLclients();
+            if (lc.size() == 0)
+                System.out.println("Liste de client vide");
+            else {
+                for (Client t : listecli) {
+                    System.out.println("________________________________");
+                    System.out.println("Numero client: " + t.getNumcli());
+                    System.out.println("Nom client: " + t.getNomcli());
+                    System.out.println("Prenom client: " + t.getPrenomcli());
+                    System.out.println("Agence du client: " + t.getAgence().getNomag());
+                }
+                System.out.println("________________________________");
+            }
+        }
+    }
+
+    public void listerClientsAgence(String num){
+        Agence ag = searchAgence(num);
+        if(ag!=null){
+            if (ag.getLclients().size()==0){
+                System.out.println("Liste des clients vides");
+            }else{
+                for(Client x: ag.getLclients()){
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Numero : "+x.getNumcli());
+                    System.out.println("Nom : "+x.getNomcli());
+                    System.out.println("Prenom : "+x.getPrenomcli());
+                    System.out.println("Nom Agence : "+ag.getNomag());
+                }
+                System.out.println("-----------------------------------------------------");
+            }
+        }else{
+            System.out.println("Agence inexistante !!!");
         }
     }
 
---
+++ src/presentation/Vue.java
@@ -0,0 +1,72 @@
+package presentation;
+
+import domaine.*;
+import dao.AccesBD;
+
+import java.util.Scanner;
+
+
+public class Vue {
+    //injection de dependance en creant une instance de la dao
+    private static AccesBD bd= new AccesBD();
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int choix = 0;
+        Agence ag = null;
+        String nomCli, prenomCli, codeAg, nomAg, numAg;
+        do{
+            System.out.println("1. Creer une agence");
+            System.out.println("2. Lister les agences");
+            System.out.println("3. Creer un client");
+            System.out.println("4. Lister les clients de la banque");
+            System.out.println("5. Lister les clients d'une agence ");
+            System.out.println("6. Exit/Sortie");
+            System.out.println("Votre choix ?");
+
+            choix=sc.nextInt();
+            sc.nextLine();
+            switch (choix){
+                case 1 :
+                    System.out.println("Code de l'agence ? ");
+                    codeAg= sc.nextLine();
+                    System.out.println("Nom de l'agence ?");
+                    nomAg= sc.nextLine();
+                    ag = new Agence(codeAg, nomAg);
+                    bd.createAg(ag);
+                    break;
+                case 2:
+                    bd.listAg();
+                    break;
+                case 3 :
+                    System.out.println("Numéro de l'agence ? ");
+                    numAg=sc.nextLine();
+                    ag = bd.searchAgence(numAg);
+                    if (ag==null)
+                        System.out.println("Agence inexistante");
+                    else  {
+                        System.out.println("Nom client ? ");
+                        nomCli= sc.nextLine();
+                        System.out.println("Prenom du client ?");
+                        prenomCli= sc.nextLine();
+                        Client c = new Client(nomCli,prenomCli);
+                        bd.saveClient(c, ag);
+                    }
+                    break;
+                case 4:
+                    bd.listAg();
+                    break;
+                case 5 :
+                    System.out.println("Numéro de l'agence ? ");
+                    numAg=sc.nextLine();
+                    bd.listerClientsAgence(numAg);
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("A bientôt !!! Merciiii");
+            }
+
+        }while(choix!=6);
+    }
+}
